224e30cd124721138e8af9d0083ce75e47c3cf94
Evan <cantis@gmail.com>
2021-06-24 21:30:20 -0500

    Before 1st assignment

--- GS01/app.js
+++ GS01/app.js
@@ -1,20 +1,17 @@
 Vue.createApp({
-    data() {
-        return {
-            goals: [],
-            enteredValue: ''
-        };
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
     },
-    methods: {
-        addGoal() {
-            this.goals.push(this.enteredValue);
-        }
-    }
-}).mount('#app');
-
-
-
-
+  },
+}).mount("#app");
 
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
@@ -29,4 +26,3 @@ Vue.createApp({
 // }
 
 // buttonEl.addEventListener('click', addGoal);
-
